04164d5df523c357f7840d9e75cd0b237d53174d
sososo <sh0000@pusan.ac.kr>
2023-01-31 18:51:26 +0900

    sohyun 4th commit

--- src/component/ui/ContentGrid.jsx
+++ src/component/ui/ContentGrid.jsx
@@ -1,15 +1,7 @@
 import React, { useState, useRef } from "react";
-import styled, { ThemeProvider } from "styled-components";
-//import theme from "../../../theme";
-import { useNavigate } from "react-router-dom";
-import { Container, CssBaseline } from '@mui/material';
 import { Box } from '@mui/material';
 import { Grid } from '@mui/material';
-import { Typography } from '@mui/material';
-import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
-import { Link } from '@mui/material';
-import { Input } from "@mui/material";
 import ContentBox from "./ContentBox";
 
 
