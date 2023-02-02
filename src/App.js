825372aa54b402d86c97947a359afcfadc419bcc
pigglehyun <sarah0661@pusan.ac.kr>
2023-02-03 02:02:41 +0900

    seung

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import MainPage from './component/page/MainPage';
 import SearchPage from './component/page/SearchPage';
 import SettingPage from './component/page/SettingPage';
 import FriendPage from './component/page/FriendPage';
+import FirstPage from './component/page/FirstPage';
 import React from 'react';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<MainPage />} />
+        <Route index element={<FirstPage />} />
+        <Route path="main" element={<MainPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="search" element={<SearchPage />}/>
         <Route path="write" element={<WritePage />}/>
