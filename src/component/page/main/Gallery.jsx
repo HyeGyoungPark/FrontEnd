19b2c7874534d57c02ddffcc4eed37279cf36da5
sososo <sh0000@pusan.ac.kr>
2023-02-04 23:17:02 +0900

    sohyun 8th commit

--- src/component/page/main/Gallery.jsx
+++ src/component/page/main/Gallery.jsx
@@ -1,27 +1,36 @@
 import React, {useState} from 'react';
 import GalleryList from './GalleryList';
-import GalleryView from './GalleryView';
-import data from './images';
-import { useNavigate } from 'react-router-dom';
+import GalleryDesc from './GalleryDesc';
 import './GalleryCss.css'
+import GalleryItem from './GalleryItem';
+import GalleryTitle from './GalleryTitle';
 
 
-function Gallery(){
-    const navigate = useNavigate();
+function Gallery(props){
 
-    const [datas, setDatas] = useState(data);       //사진 데이터
-    const [currItem,setCurrItem] = useState(datas[0]);  //선택한 사진 상태 설정
-
-    const onView = (id) => {            //고유번호 id를 받아서 사진 찾기
-        setCurrItem(datas.find(item => item.id == id))  //배열함수중 해당 값만 찾아줌
-    }
+    const {data} = props;
+    const [index, setIndex] = useState(0);
+   
 
     return(
-        <div className='wrap'>
-            <GalleryList  datas = {datas} onView = {onView} currItem = {currItem}/>
-            <GalleryView currItem = {currItem}/>
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}
+        >
+
+            <br />
+            <GalleryTitle data={data}></GalleryTitle>
+            <br />
+            <GalleryItem data={data} index={index} setIndex={setIndex}></GalleryItem>
+            <br />
+            <GalleryDesc data={data} index={index}></GalleryDesc>
+            <br />
+            
         </div>
-    )
+    );
 }
 
 export default Gallery;
