a01bba04f6b5751200446179cfc9433b4d41d2ea
phg1616 <phg1616@pusan.ac.kr>
2023-02-14 18:06:16 +0900

    댓글 위치 이동, 댓글 입력창 박스 변경

--- src/component/ui/CommentBox.jsx
+++ src/component/ui/CommentBox.jsx
@@ -6,6 +6,7 @@ import TextInput from "./TextInput";
 import postdata from "../../postData.json";
 import { Box } from '@mui/material'
 import { Button } from '@mui/material';
+import { Input } from "antd"
 
 const CommentLabel = styled.p`
     font-size: 16px;
@@ -17,16 +18,16 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    justify-content: center;
+    justify-content: bottom;
 `;
 
 
+const { TextArea } = Input
 
 function CommentBox(props) {
-    
-    console.log(props);
     const navigate = useNavigate();
     const { postId } = useParams();
+    console.log(props)
 
     const postcomment = postdata.find((item) => {
         return item.id == postId;
@@ -41,13 +42,12 @@ function CommentBox(props) {
         <CommentLabel>댓글</CommentLabel>
         <CommentList comments={props.comments}></CommentList>
         
-        <TextInput
-            height={40}
+        <TextArea 
             value={comment}
-            onChange={(event) => {
-            setComment(event.target.value);
-        }}/>
-        
+            onChange={(event) => {setComment(event.target.value);}}
+            autoSize={{minRows: 1, maxRows: 1}}
+        />
+
         <Button 
                 type="submit" 
                 variant="outlined" 
@@ -57,7 +57,7 @@ function CommentBox(props) {
                     pl: 11,
                     color: 'white',
                     border: '1px solid skyblue',
-                    borderRadius: '10px',
+                    borderRadius: '8px',
                     backgroundColor: 'skyblue',
                     // "&.Mui[mui이름]-root:[event 속성]" : {}로 기본적으로 적용된 css를 변경시킬 수 있다.
                     // "&.MuiButton-root:hover" : {}로 기본적으로 탑재되어있는 css를 바꿈
@@ -67,7 +67,7 @@ function CommentBox(props) {
                     }
                 }}
                 onClick={() => {
-                    navigate("/.");
+                    navigate("");
                 }}>댓글 작성하기</Button>
     </Box>
     </Wrapper>
